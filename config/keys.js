fee8208fbed3d329c846d66392f8e1b5554e2ebf
Reggie Burrus <reggie.burrus@outlook.com>
2019-01-03 11:51:29 -0600

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 //keys.js - figure out what credentials to return
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('.prod');
+    module.exports = require('./prod');
 } else {
   module.exports = require('./dev');
 }
