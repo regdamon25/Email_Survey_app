190607022407d1d42f97f3afef306387637b057d
Reggie Burrus <reggie.burrus@outlook.com>
2019-01-13 12:38:21 -0600

    dont know if this is going to work

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+//dev.js - don't commit this!!!
+module.exports = {
+    googleClientID: '413338476210-68ngtuikeq42r9i7dca6703ibjnr6jgj.apps.googleusercontent.com',
+    googleClientSecret: 'yXX6eWdRD6mN-3gXXKyPmtFI',
+    mongoURI:'mongodb://Regman25:102816sbr!@ds023478.mlab.com:23478/eemaily-dev',
+    cookieKey: 'aljsiurhwoccwljkjhfiuhgwiubdkzbf',
+    stripePublishableKey: 'pk_test_qrdU3hFZcswqBTkfQJGdwLW3',
+    stripeSecretKey: 'sk_test_bpEK09aCMTpuf1JpzSlHLulr'
+};
